88c5a75503fefe996fe882c2d4e5d978cc4f9445
Jason Trenouth <jason.trenouth@gmail.com>
2024-11-10 15:32:16 +0000

    Add basic update loop

--- Top/Program.cs
+++ Top/Program.cs
@@ -1,28 +1,40 @@
 ﻿using System.Diagnostics;
+using System.Reactive.Linq;
 
 namespace Top
 {
     internal class Program
     {
-        const string cFormat = "{0,-8} {1,-40} {2,-20} {3,-10}";
+        private const string cFormat = "{0,-8} {1,-40} {2,-20} {3,-10}";
 
         static void Main(string[] args)
         {
-            Process[] processlist = Process.GetProcesses();
+            IObservable<long> times = Observable.Interval(TimeSpan.FromSeconds(5)).StartWith(0);
+            IObservable<IEnumerable<Process>> processes = times.Select(_ => Generate());
+            processes.Subscribe(processes => Display(processes));
+            processes.Wait();
+        }
 
+        private static IEnumerable<Process> Generate()
+        {
+            return Process.GetProcesses().OrderByDescending(p => Safe(() => p.TotalProcessorTime)).Take(20);
+        }
+
+        private static void Display(IEnumerable<Process> processes)
+        {
             Console.Clear();
-            Console.WriteLine(cFormat, "ID", "Name", "Time", "Memory");
-            foreach (Process p in processlist)
+            Console.WriteLine(cFormat, "ID", "Name", "Time (HH:MM:SS)", "Memory (bytes)");
+            foreach (Process p in processes)
             {
                 Console.WriteLine(cFormat, p.Id.ToString(), p.ProcessName, Safe(() => p.TotalProcessorTime).ToString(), p.NonpagedSystemMemorySize64.ToString());
             }
         }
 
-        private static T Safe<T>(Func<T> property)
+        private static T Safe<T>(Func<T> func)
         {
             try
             {
-                return property();
+                return func();
             }
             catch
             {
