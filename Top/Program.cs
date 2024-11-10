8b395b742aa3be9fe007b5f99640298553e33fb9
Jason Trenouth <jason.trenouth@gmail.com>
2024-11-10 15:04:36 +0000

    Display a basic table

--- Top/Program.cs
+++ Top/Program.cs
@@ -4,14 +4,29 @@ namespace Top
 {
     internal class Program
     {
+        const string cFormat = "{0,-8} {1,-40} {2,-20} {3,-10}";
+
         static void Main(string[] args)
         {
             Process[] processlist = Process.GetProcesses();
 
             Console.Clear();
+            Console.WriteLine(cFormat, "ID", "Name", "Time", "Memory");
             foreach (Process p in processlist)
             {
-                Console.WriteLine($"Process: {p.ProcessName} ID: {p.Id}");
+                Console.WriteLine(cFormat, p.Id.ToString(), p.ProcessName, Safe(() => p.TotalProcessorTime).ToString(), p.NonpagedSystemMemorySize64.ToString());
+            }
+        }
+
+        private static T Safe<T>(Func<T> property)
+        {
+            try
+            {
+                return property();
+            }
+            catch
+            {
+                return default(T);
             }
         }
     }
