76ce7bf377bac969c9e2cc7532e8fb3ef881eac3
Jason Trenouth <jason.trenouth@gmail.com>
2024-11-10 10:33:36 +0000

    Initial commit of .NET top

---
+++ Top/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+
+namespace Top
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Process[] processlist = Process.GetProcesses();
+
+            Console.Clear();
+            foreach (Process p in processlist)
+            {
+                Console.WriteLine($"Process: {p.ProcessName} ID: {p.Id}");
+            }
+        }
+    }
+}
